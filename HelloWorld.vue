f3b2a1d2d493b2c0e9dab4ade0bbb43d5efc6a36
UnAnvious <127380913+UnAnvious@users.noreply.github.com>
2023-11-07 09:53:22 +0800

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,175 @@
+<template>
+<div class="Desktop4" style="width: 100%; height: 100%; position: relative; background: white">
+    <div class="SideBar1" style="width: 120px; height: 1117px; left: 0px; top: 0px; position: absolute">
+        <div class="Rectangle4" style="width: 120px; height: 1117px; left: 0px; top: 0px; position: absolute; background: #FFF2F2"></div>
+        <div class="Home" style="width: 40px; height: 39px; left: 40px; top: 164px; position: absolute">
+            <div class="Vector" style="width: 40px; height: 39px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 33.33px; height: 27.62px; left: 3.33px; top: 4.88px; position: absolute; background: #FF9367"></div>
+        </div>
+        <div class="Notifications" style="width: 40px; height: 40px; left: 40px; top: 284px; position: absolute">
+            <div class="Vector" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 26.67px; height: 32.50px; left: 6.67px; top: 4.17px; position: absolute; background: #FF9367"></div>
+        </div>
+        <div class="CalendarToday" style="width: 33px; height: 32px; left: 44px; top: 407px; position: absolute">
+            <div class="Vector" style="width: 33px; height: 32px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 27.50px; height: 29.33px; left: 2.75px; top: 1.33px; position: absolute; background: #FF9367"></div>
+        </div>
+        <div class="ShowChart" style="width: 34px; height: 34px; left: 43px; top: 525px; position: absolute">
+            <div class="Vector" style="width: 34px; height: 34px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 28.33px; height: 18.39px; left: 2.83px; top: 7.81px; position: absolute; background: #FF9367"></div>
+        </div>
+        <div class="Person" style="width: 34px; height: 33px; left: 43px; top: 645px; position: absolute">
+            <div class="Vector" style="width: 34px; height: 33px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 22.67px; height: 22px; left: 5.67px; top: 5.50px; position: absolute; background: #FF9367"></div>
+        </div>
+        <div class="Group1" style="width: 65px; height: 65px; left: 28px; top: 979px; position: absolute">
+            <div class="Ellipse1" style="width: 65px; height: 65px; left: 0px; top: 0px; position: absolute; background: #FF9367; border-radius: 9999px"></div>
+            <div class="Settings" style="width: 38.52px; height: 38.52px; left: 12.84px; top: 12.84px; position: absolute">
+                <div class="Group" style="width: 38.52px; height: 38.52px; left: 0px; top: 0px; position: absolute">
+                    <div class="Vector" style="width: 38.52px; height: 38.52px; left: 0px; top: 0px; position: absolute"></div>
+                    <div class="Vector" style="width: 29.97px; height: 30.81px; left: 4.27px; top: 3.85px; position: absolute; background: white"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="SideBar2" style="width: 376px; height: 1117px; left: 134px; top: 0px; position: absolute">
+        <div class="Settings" style="left: 88px; top: 41px; position: absolute; color: #1C1C1C; font-size: 32px; font-family: Roboto; font-weight: 700; word-wrap: break-word">settings</div>
+        <div class="KeyboardArrowLeft" style="width: 48px; height: 48px; left: 0px; top: 35px; position: absolute">
+            <div class="Vector" style="width: 48px; height: 48px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 14.82px; height: 24px; left: 16px; top: 9px; position: absolute; background: black"></div>
+        </div>
+        <div class="Group2" style="width: 197px; height: 45px; left: 56px; top: 164px; position: absolute">
+            <div class="EditProfil" style="left: 73px; top: 6px; position: absolute; color: #1C1C1C; font-size: 28px; font-family: Roboto; font-weight: 700; word-wrap: break-word">Edit Profil</div>
+            <div class="Edit" style="width: 45px; height: 45px; left: 0px; top: 0px; position: absolute">
+                <div class="Vector" style="width: 45px; height: 45px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 33.75px; height: 33.75px; left: 5.62px; top: 5.62px; position: absolute; background: #1C1C1C"></div>
+            </div>
+        </div>
+        <div class="Group3" style="width: 191px; height: 40px; left: 56px; top: 291px; position: absolute">
+            <div class="Notifikasi" style="left: 71px; top: 7px; position: absolute; color: #858585; font-size: 28px; font-family: Roboto; font-weight: 500; word-wrap: break-word">Notifikasi</div>
+            <div class="Notifications" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute">
+                <div class="Vector" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 26.67px; height: 32.50px; left: 6.67px; top: 4.17px; position: absolute; background: #858585"></div>
+            </div>
+        </div>
+        <div class="Group4" style="width: 205px; height: 40px; left: 56px; top: 413px; position: absolute">
+            <div class="Lock" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute">
+                <div class="Group" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute">
+                    <div class="Vector" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute"></div>
+                    <div class="Vector" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute; opacity: 0.87"></div>
+                </div>
+                <div class="Vector" style="width: 26.67px; height: 35px; left: 6.67px; top: 1.67px; position: absolute; background: #858585"></div>
+            </div>
+            <div class="Keamanan" style="left: 71px; top: 7px; position: absolute; color: #858585; font-size: 28px; font-family: Roboto; font-weight: 500; word-wrap: break-word">Keamanan</div>
+        </div>
+        <div class="Group6" style="width: 175px; height: 40px; left: 56px; top: 535px; position: absolute">
+            <div class="HelpOutline" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute">
+                <div class="Vector" style="width: 40px; height: 40px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 33.33px; height: 33.33px; left: 3.33px; top: 3.33px; position: absolute; background: #858585"></div>
+            </div>
+            <div class="Bantuan" style="left: 71px; top: 7px; position: absolute; color: #858585; font-size: 28px; font-family: Roboto; font-weight: 500; word-wrap: break-word">Bantuan</div>
+        </div>
+        <div class="Line1" style="width: 1117px; height: 0px; left: 376px; top: 0px; position: absolute; transform: rotate(90deg); transform-origin: 0 0; border: 1px #858585 solid"></div>
+    </div>
+    <div class="Profile1" style="width: 104px; height: 40px; left: 1760px; top: 40px; position: absolute">
+        <img class="UnsplashC8ta0gwpbqg" style="width: 40px; height: 40px; left: 64px; top: 0px; position: absolute; border-radius: 500px" src="https://via.placeholder.com/40x40" />
+        <div class="Group7" style="width: 38px; height: 38px; left: 0px; top: 1px; position: absolute">
+            <div class="Ellipse2" style="width: 38px; height: 38px; left: 0px; top: 0px; position: absolute; background: #D9D9D9; border-radius: 9999px"></div>
+            <div class="Notifications" style="width: 30px; height: 30px; left: 4px; top: 4px; position: absolute">
+                <div class="Vector" style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 20px; height: 24.38px; left: 5px; top: 3.12px; position: absolute; background: #1C1C1C"></div>
+            </div>
+        </div>
+    </div>
+    <img class="UnsplashC8ta0gwpbqg" style="width: 120px; height: 120px; left: 1413px; top: 56px; position: absolute; border-radius: 500px" src="https://via.placeholder.com/120x120" />
+    <div class="EditProfil" style="width: 232px; height: 53px; left: 660px; top: 83px; position: absolute; color: black; font-size: 45px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Edit profil</div>
+    <div class="Group9" style="width: 180px; height: 55px; left: 660px; top: 967px; position: absolute">
+        <div class="Rectangle10" style="width: 180px; height: 55px; left: 0px; top: 0px; position: absolute; background: white; border-radius: 5px; border: 2px #757575 solid"></div>
+        <div class="Cancel" style="left: 47px; top: 11px; position: absolute; color: #757575; font-size: 28px; font-family: Roboto; font-weight: 400; word-wrap: break-word">Cancel</div>
+    </div>
+    <div class="Group10" style="width: 180px; height: 55px; left: 896px; top: 967px; position: absolute">
+        <div class="Rectangle10" style="width: 180px; height: 55px; left: 0px; top: 0px; position: absolute; background: #757575; border-radius: 5px"></div>
+        <div class="Save" style="left: 60px; top: 11px; position: absolute; color: white; font-size: 28px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Save</div>
+    </div>
+    <div class="Frame" style="width: 52.01px; height: 52px; left: 34px; top: 33px; position: absolute">
+        <div class="Icon0" style="width: 52.01px; height: 52px; left: 0px; top: 0px; position: absolute">
+            <div class="Vector" style="width: 52.01px; height: 52px; left: 0px; top: -0px; position: absolute; background: #FF9367"></div>
+        </div>
+    </div>
+    <div class="Firstname" style="width: 873px; height: 108px; left: 660px; top: 203px; position: absolute">
+        <div class="Rectangle7" style="width: 873px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="Nama" style="left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Nama</div>
+        <div class="Erik" style="left: 27px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">erik</div>
+    </div>
+    <div class="City" style="width: 876px; height: 108px; left: 660px; top: 707px; position: absolute">
+        <div class="Rectangle7" style="width: 876px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="City" style="width: 46px; left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">City</div>
+        <div class="Makassar" style="width: 264px; left: 27px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">Makassar</div>
+        <div class="KeyboardArrowDown" style="width: 48px; height: 48px; left: 807px; top: 49px; position: absolute">
+            <div class="Vector" style="width: 48px; height: 48px; left: 0px; top: 0px; position: absolute"></div>
+            <div class="Vector" style="width: 24px; height: 14.82px; left: 12px; top: 15.18px; position: absolute; background: #1C1C1C"></div>
+        </div>
+    </div>
+    <div class="Email" style="width: 873px; height: 108px; left: 660px; top: 329px; position: absolute">
+        <div class="Rectangle7" style="width: 873px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="Email" style="width: 275.91px; left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Email</div>
+        <div class="ErikGmailCom" style="width: 407.40px; left: 27.09px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">erik@gmail.com</div>
+        <div class="Group8" style="width: 35.43px; height: 35.63px; left: 815.80px; top: 55px; position: absolute">
+            <div class="Rectangle9" style="width: 29.52px; height: 29.69px; left: 0px; top: 5.94px; position: absolute; background: #23B000; border-radius: 5px"></div>
+            <div class="Check" style="width: 35.43px; height: 35.63px; left: 0px; top: 0px; position: absolute">
+                <div class="Vector" style="width: 35.43px; height: 35.63px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 19.19px; height: 14.85px; left: 5.17px; top: 13.36px; position: absolute; background: white"></div>
+            </div>
+        </div>
+    </div>
+    <div class="Password" style="width: 876px; height: 108px; left: 660px; top: 833px; position: absolute">
+        <div class="Rectangle7" style="width: 876px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="Password" style="width: 275.91px; left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Password</div>
+        <div style="width: 407.40px; left: 27.09px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">***************</div>
+        <div class="Group8" style="width: 35.43px; height: 35.63px; left: 815.79px; top: 55px; position: absolute">
+            <div class="Rectangle9" style="width: 29.52px; height: 29.69px; left: 0px; top: 5.94px; position: absolute; background: #23B000; border-radius: 5px"></div>
+            <div class="Check" style="width: 35.43px; height: 35.63px; left: 0px; top: 0px; position: absolute">
+                <div class="Vector" style="width: 35.43px; height: 35.63px; left: 0px; top: 0px; position: absolute"></div>
+                <div class="Vector" style="width: 19.19px; height: 14.85px; left: 5.17px; top: 13.36px; position: absolute; background: white"></div>
+            </div>
+        </div>
+    </div>
+    <div class="Address" style="width: 876px; height: 108px; left: 660px; top: 455px; position: absolute">
+        <div class="Rectangle7" style="width: 876px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="Address" style="width: 276.86px; left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Address</div>
+        <div class="JlCenderawasih" style="width: 408.80px; left: 27.19px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">Jl. Cenderawasih</div>
+    </div>
+    <div class="Address" style="width: 876px; height: 108px; left: 660px; top: 581px; position: absolute">
+        <div class="Rectangle7" style="width: 876px; height: 70px; left: 0px; top: 38px; position: absolute; background: white; border-radius: 5px; border: 2px #858585 solid"></div>
+        <div class="ContactNumber" style="width: 276.86px; left: 0px; top: 0px; position: absolute; color: #1C1C1C; font-size: 26px; font-family: Roboto; font-weight: 600; word-wrap: break-word">Contact Number</div>
+        <div class="6212345678901" style="width: 408.80px; left: 27.19px; top: 59px; position: absolute; color: #858585; font-size: 24px; font-family: Roboto; font-weight: 500; word-wrap: break-word">+62 1234 5678 901</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
